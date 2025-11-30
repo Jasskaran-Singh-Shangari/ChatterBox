8dc3a5795bd3556c0fad02e44aaa42700fe25858
Jasskaran Singh Shangari <jasskaransingh0007@gmail.com>
2025-11-30 19:22:32 +0530

    Minor changes

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5002/api",
+    baseURL: "https://chatter-box-qvoy.onrender.com/api",
     withCredentials: true,
     credentials: true
 })
